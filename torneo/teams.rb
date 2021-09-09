a9da4b2a40ab3f3b1fdec611bf844bf2e6a7b1ed
Rodrigo <oyarzun.rodrigoant@gmail.com>
2021-09-08 22:18:03 -0300

    Update teams.rb
    claculo puntos totales team

--- torneo/teams.rb
+++ torneo/teams.rb
@@ -5,10 +5,17 @@ class Teams
         @code_team = code_team
         @name_team = name_team
         @jugadores = Array.new
+        @total_points = 0
     end
     def loadjugadores code, name, age, rut, color, match_win, match_draw, match_lose
         jugador = Jugador.new code, name, age, rut, color, match_win, match_draw, match_lose
         @jugadores.push(jugador)
         @jugadores
+    end
+    def calculate_total_points
+        for jugador in @jugadores
+            @total_points = @total_points + jugador.player_points
+        end
+        @total_points
     end
 end
