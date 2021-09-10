27a3db8a792f9341161ebea272569d7c43477724
Rodrigo <oyarzun.rodrigoant@gmail.com>
2021-09-10 00:11:13 -0300

    Update jugador.rb

--- torneo/jugador.rb
+++ torneo/jugador.rb
@@ -1,5 +1,5 @@
 class Jugador
-    attr_accessor :code, :name, :age, :rut, :color, :match_win, :match_draw, :match_lose
+    attr_accessor :code, :name, :age, :rut, :color, :match_win, :match_draw, :match_lose, :player_points
     def initialize (code, name, age, rut, color, match_win, match_draw, match_lose)
         @code = code
         @name = name
@@ -9,8 +9,9 @@ class Jugador
         @match_win = match_win
         @match_draw = match_draw
         @match_lose = match_lose
+        @player_points = 0
     end
-    def player_points 
-        player_points = @match_win*4 + @match_draw*3
+    def calculate_player_points 
+        @player_points = @match_win*4 + @match_draw*3
     end
 end
